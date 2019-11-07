1a0cd154c25ff7fc5802dbd55d9f110eb9af5a3c
Carl Davidson <carl.davidson@hotmail.com>
2019-11-07 17:02:06 +1000

    finished adding devise

--- src/app/controllers/orders_controller.rb
+++ src/app/controllers/orders_controller.rb
@@ -1,7 +1,33 @@
+require 'stripe'
+
 class OrdersController < ApplicationController
   def new
     @tattoo = Tattoo.find(params[:tattoo_id])
     
-    Stripe.api_key = ''
+    Stripe.api_key = Rails.application.credentials.dig(:stripe, :api_key)
+    @session = Stripe::Checkout::Session.create(
+      payment_method_types: ['card'],
+      line_items: [{
+        name: @tattoo.title,
+        description: @tattoo.description,
+        images: [],
+        amount: (@tattoo.price * 100).to_i,
+        currency: 'aud',
+        quantity: 1
+      }],
+      success_url: "#{root_url}/orders/complete",
+      cancel_url: "#{root_url}/orders/complete",
+    )
+  end
+
+  def complete
+    flash[:alert] = 'Your order has been processed successfully.'
+    redirect_to tattoos_path
+    # render html: '<h2>Thanks for your order!</h2>'.html_safe
+  end
+
+  def cancel
+    redirect_to tattoos_path, notice: 'Your order has been cancelled!'
+    # render html: '<h2>Your order has been cancelled!</h2>'.html_safe
   end
 end
--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'orders/complete', to: "orders#complete", as: "orders_complete"
+  get 'orders/cancel'
   get 'orders/:tattoo_id', to: "orders#new", as: "new_order"
+
   resources :store
   resources :tattoos
   
